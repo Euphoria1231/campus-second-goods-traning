bf0c56a3b5739a4054793e6fc1573c778ff2489e
TangSY <357770@whut.edu.cn>
2025-10-11 14:52:03 +0800

    feat: init

---
+++ campus-second-goods-traning/src/main/java/com/campus/service/Impl/LoginServiceImpl.java
@@ -0,0 +1,7 @@
+package com.campus.service.Impl;
+
+import org.springframework.stereotype.Service;
+import com.campus.service.LoginService;
+@Service
+public class LoginServiceImpl implements LoginService {
+}
